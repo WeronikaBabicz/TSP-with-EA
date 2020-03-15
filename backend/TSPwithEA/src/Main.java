af50870976e3dbe69280fb166c773dd4e77428e6
Weronika <weronikababicz1@gmail.com>
2020-03-15 02:41:55 +0100

    Changed main where data loads properly and ProblemType is set

--- backend/TSPwithEA/src/Main.java
+++ backend/TSPwithEA/src/Main.java
@@ -1,8 +1,24 @@
+import algorithms.EvolutionAlgorithm;
+import algorithms.TSPProblem;
+import algorithms.TSPProblemGeo;
+import algorithms.TSPProblemPlanar;
 import dataParser.*;
 
 public class Main {
     public static void main(String[] args) {
         Parser parser = new Parser();
-        ProblemData x = parser.parse("fl417.tsp");
+        ProblemData x = parser.parse("pr2392.tsp");
+        TSPProblem problem = null;
+
+        switch (x.problemType){
+            case PLANAR:
+                problem = new TSPProblemPlanar(x.points);
+                break;
+            case GEO:
+                problem = new TSPProblemGeo(x.points);
+                break;
+        }
+
+        EvolutionAlgorithm ea = new EvolutionAlgorithm(problem);
     }
 }
