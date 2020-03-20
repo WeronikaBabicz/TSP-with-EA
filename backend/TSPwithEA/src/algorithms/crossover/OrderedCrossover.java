3ac650dbab7a71089b5b1bc1cd6ba203fd2a8b07
Weronika <weronikababicz1@gmail.com>
2020-03-20 21:00:20 +0100

    Fixed changing genes in OrderedCrossover

--- backend/TSPwithEA/src/algorithms/crossover/OrderedCrossover.java
+++ backend/TSPwithEA/src/algorithms/crossover/OrderedCrossover.java
@@ -15,18 +15,21 @@ public class OrderedCrossover implements Crossover{
         int idx2 = generateRandomIndex(parent1.getGenotype());
 
         changeGenes(genotype, Math.min(idx1, idx2), Math.max(idx1, idx2), parent2);
-
         offspring.setGenotype(genotype);
         return offspring;
     }
 
     private void changeGenes(ArrayList<Integer> genotype, int startIndex, int endIndex, Individual parent2){
-        List<Integer> notUsedGenes = ((List<Integer>) parent2.getGenotype().clone());
+        //List<Integer> notUsedGenes = ((List<Integer>) parent2.getGenotype().clone());
+        List<Integer> notUsedGenes = new ArrayList<>(parent2.getGenotype());
+
         notUsedGenes.removeIf((genotype.subList(startIndex, endIndex + 1)::contains)); // [1 2 3 4] sub(1,3): [2 3]
 
-        for (int i = 0; i < genotype.size() && (i < startIndex || i > endIndex); i++){
-            genotype.set(i, notUsedGenes.get(0)); //TODO: works?
-            notUsedGenes.remove(0);
+        for (int i = 0; i < genotype.size(); i++){
+            if (i < startIndex || i > endIndex){
+                genotype.set(i, notUsedGenes.get(0)); //TODO: works?
+                notUsedGenes.remove(0);
+            }
         }
     }
 
